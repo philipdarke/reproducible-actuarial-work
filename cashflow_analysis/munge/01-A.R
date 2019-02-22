a9082e34aab4cbe71f2a8d3de7808d2a293de7e7
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2019-02-22 14:49:40 +0000

    Add data pre-processing to cashflow_analysis

--- cashflow_analysis/munge/01-A.R
+++ cashflow_analysis/munge/01-A.R
@@ -1,1 +1,8 @@
-# Example preprocessing script.
+# Remove first column of rawcashflows.csv
+cashflows_to_use = rawcashflows[-1]
+
+# Sum the rows of the rawcashflow table and store them in the variable cashflows
+cashflows = apply(cashflows_to_use, 1, sum)
+
+# Convert cashflows to a time series object (monthly frequency starting 01/2019)
+cashflows = ts(cashflows, start=c(2019,1), frequency=12)
